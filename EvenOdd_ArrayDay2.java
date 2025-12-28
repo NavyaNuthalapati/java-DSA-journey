2452ad62ea71230d3908f9feff38a713ca678520
NavyaNuthalapati <149475968+NavyaNuthalapati@users.noreply.github.com>
2025-12-28 12:53:23 +0530

    Add files via upload
    JAVA-DSA-Day2

---
+++ EvenOdd_ArrayDay2.java
@@ -0,0 +1,19 @@
+public class EvenOdd_ArrayDay2 {
+    public static void evenOdd(int arr[]){
+         int sumEven=0;
+         int sumOdd=0;
+         for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                sumEven++;
+            }else{
+                sumOdd++;
+            }
+         }
+         System.out.println("The count of even numbers in this array are: "+sumEven);
+          System.out.println("The count of odd numbers in this array are: "+sumOdd);
+    }    
+    public static void main(String args[]){
+        int arr[]={2,3,4,17,16,26,16,33,99,100};
+        evenOdd(arr);
+    }
+}

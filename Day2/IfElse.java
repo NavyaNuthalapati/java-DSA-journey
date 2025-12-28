f3fd04718c24a728a29689016b446a7ded2b9bc8
NavyaNuthalapati <149475968+NavyaNuthalapati@users.noreply.github.com>
2025-12-28 13:21:34 +0530

    Added IfElse.java code to Day2

---
+++ Day2/IfElse.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class IfElse {
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int num=scn.nextInt();
+          
+        if(num>0){
+            System.out.println(num+" Is a Positive Integer");
+          }
+        else if(num<0){
+            System.out.println(num+" Is a Negative Integer");
+          }
+        else{
+            System.out.println(num+" Is a ZERO");
+            }
+         
+    }
+    
+ }

783f053b920e0b936b82760a6cb0aef218ef2482
NavyaNuthalapati <149475968+NavyaNuthalapati@users.noreply.github.com>
2025-12-28 14:30:15 +0530

    Update EvenOdd_Array.java code without using functions

--- Day2/EvenOdd_Array.java
+++ Day2/EvenOdd_Array.java
@@ -1,8 +1,10 @@
-public class EvenOdd_Array{
-    public static void evenOdd(int arr[]){
+public class EvenOdd_Array{  
+    public static void main(String args[]){
+        int arr[]={2,3,4,17,16,26,16,33,99,100};
          int sumEven=0;
          int sumOdd=0;
-         for(int i=0;i<arr.length;i++){
+         for(int i=0;i<arr.length;i++)
+         {
             if(arr[i]%2==0){
                 sumEven++;
             }else{
@@ -11,9 +13,5 @@ public class EvenOdd_Array{
          }
          System.out.println("The count of even numbers in this array are: "+sumEven);
           System.out.println("The count of odd numbers in this array are: "+sumOdd);
-    }    
-    public static void main(String args[]){
-        int arr[]={2,3,4,17,16,26,16,33,99,100};
-        evenOdd(arr);
     }
 }
